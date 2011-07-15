f351ead61ffff4709e6892134d329cffe7e644f8
Giammy <giangiammy@gmail.com>
2011-07-15 14:35:31 +0200

    initial commit

---
+++ src/bci/tools/bscan/BscanActivity.java
@@ -0,0 +1,55 @@
+package bci.tools.bscan;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+public class BscanActivity extends Activity {
+	
+	String TAG = "BTEST";
+	int REQUEST_ENABLE_BT = 1;
+	
+	public void startUsingBT() {
+		Log.i(TAG, "OK - Start using Bluetooth"); 
+	}
+	
+	protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+		if (requestCode == REQUEST_ENABLE_BT) {
+			if (resultCode == RESULT_OK) {
+				Log.i(TAG, "Bluetooth enable OK.");
+			} else {
+				Log.i(TAG, "Bluetooth enable NO.");
+			}
+		}
+	}
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        Log.i(TAG, "Start Bluetooth test."); 
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+        	Log.i(TAG, "Device does not support Bluetooth");
+        } else {
+        	Log.i(TAG, "OK - Device supports Bluetooth");       	
+        	
+        	try {
+        		if (!mBluetoothAdapter.isEnabled()) {
+        			Log.i(TAG, "Enabling BlueTooth");
+        			Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        			startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        			Log.i(TAG, "Enabling BlueTooth - asked");
+        		}
+        	} catch (Exception e) {
+        		Log.i(TAG, "java.lang.SecurityException: " + e.getMessage());
+        	}
+        }
+              
+        Log.i(TAG, "End onCreate.");
+    }
+}
