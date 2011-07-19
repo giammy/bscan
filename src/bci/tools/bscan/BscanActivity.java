11fb42b79210baa56b49939a11cb26a9acc528e0
Gianluca Moro <giangiammy@gmail.com>
2011-07-19 10:21:57 +0200

    Added GPL license

--- src/bci/tools/bscan/BscanActivity.java
+++ src/bci/tools/bscan/BscanActivity.java
@@ -1,12 +1,23 @@
 /*
- * BSCAN - Bluetooth scanning tool
+ *  BSCAN - Bluetooth scanning tool
  * 
- * Here a Standard GPL header (to add ...)
- * 
- * Gianluca Moro - giangiammy@gmail.com
+ *  (C) 2011 - Gianluca Moro - giangiammy@gmail.com
+ *
+ *  This file is part of Bscan.
+ *
+ *  Bscan is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Bscan is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Bscan.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-/* See GPL site */
 
 package bci.tools.bscan;
 
